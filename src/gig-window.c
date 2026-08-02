0adfd78e60e28c39cb2988c5430d07b7eb92913e
Vitaly Dyachkov <obyknovenius@me.com>
2026-08-02 13:12:01 +0000

    Use Blueprint for UI

--- src/gig-window.c
+++ src/gig-window.c
@@ -12,16 +12,19 @@ G_DEFINE_TYPE (GigWindow, gig_window, ADW_TYPE_APPLICATION_WINDOW)
 static void
 gig_window_class_init (GigWindowClass *klass)
 {
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/obyknovenius/Gig/ui/gig-window.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, GigWindow, web_view);
+
+  g_type_ensure (WEBKIT_TYPE_WEB_VIEW);
 }
 
 static void
 gig_window_init (GigWindow *self)
 {
-  gtk_window_set_title (GTK_WINDOW (self), "Gig");
-  gtk_window_set_default_size (GTK_WINDOW (self), 1024, 768);
-
-  self->web_view = WEBKIT_WEB_VIEW (webkit_web_view_new ());
-  adw_application_window_set_content (ADW_APPLICATION_WINDOW (self), GTK_WIDGET (self->web_view));
+  gtk_widget_init_template (GTK_WIDGET (self));
 }
 
 GigWindow *
