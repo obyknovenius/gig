7e431b056f8aad57b4f7af20bc9ecfd0b9f79b63
Vitaly Dyachkov <obyknovenius@me.com>
2026-08-02 11:09:26 +0000

    Handle open

--- src/gig-application.c
+++ src/gig-application.c
@@ -10,6 +10,20 @@ struct _GigApplication
 G_DEFINE_TYPE (GigApplication, gig_application, ADW_TYPE_APPLICATION)
 
 static void
+gig_application_constructed (GObject *object)
+{
+  GigApplication *self = GIG_APPLICATION (object);
+
+  g_assert (GIG_IS_APPLICATION (self));
+
+  g_application_set_application_id (G_APPLICATION (self), "com.github.obyknovenius.Gig");
+  g_application_set_flags (G_APPLICATION (self), G_APPLICATION_HANDLES_OPEN);
+  g_application_set_option_context_parameter_string (G_APPLICATION (self), "[FILES…]");
+
+  G_OBJECT_CLASS (gig_application_parent_class)->constructed (object);
+}
+
+static void
 gig_application_activate (GApplication *application)
 {
   GigWindow *window;
@@ -26,11 +40,39 @@ gig_application_activate (GApplication *application)
 }
 
 static void
+gig_application_open (GApplication *application,
+                      GFile **files,
+                      gint n_files,
+                      const gchar *hint)
+{
+  GigWindow *window;
+  WebKitWebView *web_view;
+
+  g_assert (GIG_IS_APPLICATION (application));
+
+  for (gint i = 0; i < n_files; i++)
+    {
+      g_autofree gchar *uri = g_file_get_uri (files[i]);
+
+      window = gig_window_new (GTK_APPLICATION (application));
+
+      web_view = gig_window_get_web_view (GIG_WINDOW (window));
+      webkit_web_view_load_uri (web_view, uri);
+
+      gtk_window_present (GTK_WINDOW (window));
+    }
+}
+
+static void
 gig_application_class_init (GigApplicationClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GApplicationClass *application_class = G_APPLICATION_CLASS (klass);
 
+  object_class->constructed = gig_application_constructed;
+
   application_class->activate = gig_application_activate;
+  application_class->open = gig_application_open;
 }
 
 static void
@@ -41,8 +83,5 @@ gig_application_init (GigApplication *self)
 GigApplication *
 gig_application_new (void)
 {
-  return g_object_new (GIG_TYPE_APPLICATION,
-                       "application-id", "com.github.obyknovenius.Gig",
-                       "flags", G_APPLICATION_DEFAULT_FLAGS,
-                       NULL);
+  return g_object_new (GIG_TYPE_APPLICATION, NULL);
 }
