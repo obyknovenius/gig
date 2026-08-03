bd8e4b4dd1c60ee9784397c1f6de41cd990fe330
Vitaly Dyachkov <vitaly@igalia.com>
2026-08-03 08:37:36 +0000

    Add back/forward actions

--- src/gig-window.c
+++ src/gig-window.c
@@ -11,6 +11,54 @@ struct _GigWindow
 G_DEFINE_TYPE (GigWindow, gig_window, ADW_TYPE_APPLICATION_WINDOW)
 
 static void
+gig_window_go_back (GSimpleAction *action,
+                    GVariant *parameter,
+                    gpointer user_data)
+{
+  GigWindow *self = GIG_WINDOW (user_data);
+
+  g_assert (GIG_IS_WINDOW (self));
+
+  webkit_web_view_go_back (self->web_view);
+}
+
+static void
+gig_window_go_forward (GSimpleAction *action,
+                       GVariant *parameter,
+                       gpointer user_data)
+{
+  GigWindow *self = GIG_WINDOW (user_data);
+
+  g_assert (GIG_IS_WINDOW (self));
+
+  webkit_web_view_go_forward (self->web_view);
+}
+
+static const GActionEntry actions[] = {
+  { "go-back", gig_window_go_back },
+  { "go-forward", gig_window_go_forward },
+};
+
+// FIXME: Bind "can-go-back" and "can-go-forward" properties when implemented.
+static void
+gig_window_update_navigation_actions (GigWindow *self)
+{
+  GAction *action;
+
+  g_assert (GIG_IS_WINDOW (self));
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self), "go-back");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action),
+                               self->web_view ? webkit_web_view_can_go_back (self->web_view)
+                                              : FALSE);
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self), "go-forward");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action),
+                               self->web_view ? webkit_web_view_can_go_forward (self->web_view)
+                                              : FALSE);
+}
+
+static void
 on_url_entry_activated (GigWindow *self,
                         GtkEntry *entry)
 {
@@ -38,11 +86,41 @@ on_web_view_uri_changed (GigWindow *self,
 }
 
 static void
+gig_window_constructed (GObject *object)
+{
+  GigWindow *self = GIG_WINDOW (object);
+  WebKitBackForwardList *back_forward_list;
+  GAction *action;
+
+  g_assert (GIG_IS_WINDOW (self));
+
+  G_OBJECT_CLASS (gig_window_parent_class)->constructed (object);
+
+  g_action_map_add_action_entries (G_ACTION_MAP (self), actions,
+                                   G_N_ELEMENTS (actions), self);
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self), "go-back");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), FALSE);
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self), "go-forward");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), FALSE);
+
+  back_forward_list = webkit_web_view_get_back_forward_list (self->web_view);
+  g_signal_connect_object (back_forward_list, "changed",
+                           G_CALLBACK (gig_window_update_navigation_actions),
+                           self, G_CONNECT_SWAPPED);
+}
+
+static void
 gig_window_class_init (GigWindowClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/obyknovenius/Gig/ui/gig-window.ui");
+  object_class->constructed = gig_window_constructed;
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/com/github/obyknovenius/Gig/ui/gig-window.ui");
 
   gtk_widget_class_bind_template_child (widget_class, GigWindow, url_entry);
   gtk_widget_class_bind_template_child (widget_class, GigWindow, web_view);
