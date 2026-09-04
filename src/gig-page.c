f28ef980d2461817c49dde383be34581f7d3b03e
Vitaly Dyachkov <obyknovenius@me.com>
2026-09-04 13:24:24 +0000

    Create web view at runtime

--- src/gig-page.c
+++ src/gig-page.c
@@ -14,6 +14,7 @@ G_DEFINE_FINAL_TYPE (GigPage, gig_page, GTK_TYPE_WIDGET)
 enum
 {
   PROP_0,
+  PROP_WEB_VIEW,
   PROP_TITLE,
   PROP_ICON,
   PROP_IS_LOADING,
@@ -63,15 +64,79 @@ web_view_is_loading_changed_cb (GigPage *self,
 }
 
 static void
+gig_page_constructed (GObject *object)
+{
+  GigPage *self = GIG_PAGE (object);
+
+  G_OBJECT_CLASS (gig_page_parent_class)->constructed (object);
+
+  g_assert (self->web_view != NULL);
+
+  gtk_widget_set_hexpand (GTK_WIDGET (self->web_view), TRUE);
+  gtk_widget_set_vexpand (GTK_WIDGET (self->web_view), TRUE);
+
+  gtk_widget_set_parent (GTK_WIDGET (self->web_view), GTK_WIDGET (self));
+
+  g_signal_connect_object (self->web_view,
+                           "notify::uri",
+                           G_CALLBACK (web_view_uri_changed_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  g_signal_connect_object (self->web_view,
+                           "notify::title",
+                           G_CALLBACK (web_view_title_changed_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  g_signal_connect_object (self->web_view,
+                           "notify::favicon",
+                           G_CALLBACK (web_view_favicon_changed_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  g_signal_connect_object (self->web_view,
+                           "notify::is-loading",
+                           G_CALLBACK (web_view_is_loading_changed_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+}
+
+static void
 gig_page_dispose (GObject *object)
 {
   GigPage *self = GIG_PAGE (object);
 
-  g_signal_handlers_disconnect_by_data (self->web_view, self);
+  if (self->web_view)
+    {
+      g_signal_handlers_disconnect_by_data (self->web_view, self);
 
-  gtk_widget_dispose_template (GTK_WIDGET (self), GIG_TYPE_PAGE);
+      g_clear_pointer ((GtkWidget **) &self->web_view, gtk_widget_unparent);
+    }
 
   G_OBJECT_CLASS (gig_page_parent_class)->dispose (object);
+}
+
+static void
+gig_page_set_property (GObject *object,
+                       guint prop_id,
+                       const GValue *value,
+                       GParamSpec *pspec)
+{
+  GigPage *self = GIG_PAGE (object);
+  WebKitWebView *web_view = NULL;
+
+  switch (prop_id)
+    {
+    case PROP_WEB_VIEW:
+      web_view = g_value_get_object (value);
+      g_assert (web_view != NULL);
+      self->web_view = web_view;
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
 }
 
 static void
@@ -84,6 +149,9 @@ gig_page_get_property (GObject *object,
 
   switch (prop_id)
     {
+    case PROP_WEB_VIEW:
+      g_value_set_object (value, gig_page_get_web_view (self));
+      break;
     case PROP_TITLE:
       g_value_set_string (value, gig_page_get_title (self));
       break;
@@ -118,9 +186,17 @@ gig_page_class_init (GigPageClass *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
+  object_class->constructed = gig_page_constructed;
   object_class->dispose = gig_page_dispose;
+  object_class->set_property = gig_page_set_property;
   object_class->get_property = gig_page_get_property;
   widget_class->grab_focus = gig_page_grab_focus;
+
+  properties[PROP_WEB_VIEW] =
+      g_param_spec_object ("web-view",
+                           NULL, NULL,
+                           WEBKIT_TYPE_WEB_VIEW,
+                           G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
 
   properties[PROP_TITLE] =
       g_param_spec_string ("title", NULL, NULL,
@@ -142,48 +218,21 @@ gig_page_class_init (GigPageClass *klass)
   g_object_class_install_properties (object_class, N_PROPS, properties);
 
   gtk_widget_class_set_layout_manager_type (widget_class, GTK_TYPE_BIN_LAYOUT);
-
-  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/obyknovenius/Gig/ui/gig-page.ui");
-
-  gtk_widget_class_bind_template_child (widget_class, GigPage, web_view);
-
-  g_type_ensure (WEBKIT_TYPE_WEB_VIEW);
 }
 
 static void
 gig_page_init (GigPage *self)
 {
-  gtk_widget_init_template (GTK_WIDGET (self));
-
-  g_signal_connect_object (self->web_view,
-                           "notify::uri",
-                           G_CALLBACK (web_view_uri_changed_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
-
-  g_signal_connect_object (self->web_view,
-                           "notify::title",
-                           G_CALLBACK (web_view_title_changed_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
-
-  g_signal_connect_object (self->web_view,
-                           "notify::favicon",
-                           G_CALLBACK (web_view_favicon_changed_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
-
-  g_signal_connect_object (self->web_view,
-                           "notify::is-loading",
-                           G_CALLBACK (web_view_is_loading_changed_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
 }
 
 GigPage *
 gig_page_new (void)
 {
-  return g_object_new (GIG_TYPE_PAGE, NULL);
+  GtkWidget *web_view = webkit_web_view_new ();
+
+  return g_object_new (GIG_TYPE_PAGE,
+                       "web-view", web_view,
+                       NULL);
 }
 
 WebKitWebView *
--- src/gig-page.h
+++ src/gig-page.h
@@ -9,9 +9,9 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (GigPage, gig_page, GIG, PAGE, GtkWidget)
 
 GigPage *gig_page_new (void);
+WebKitWebView *gig_page_get_web_view (GigPage *self);
 const gchar *gig_page_get_title (GigPage *self);
 GdkTexture *gig_page_get_icon (GigPage *self);
 gboolean gig_page_get_is_loading (GigPage *self);
-WebKitWebView *gig_page_get_web_view (GigPage *self);
 
 G_END_DECLS
