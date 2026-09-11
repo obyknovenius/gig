e921a3de2adaf93b0d0163781343b2002dae8eb0
Vitaly Dyachkov <obyknovenius@me.com>
2026-09-11 13:02:36 +0000

    Don't show "Open Link" context menu item

--- src/gig-window.c
+++ src/gig-window.c
@@ -50,6 +50,43 @@ web_view_create_cb (GigWindow *self,
   return WEBKIT_WEB_VIEW (web_view);
 }
 
+static WebKitContextMenuItem *
+find_item_in_context_menu (WebKitContextMenu *context_menu,
+                           WebKitContextMenuAction action)
+{
+  GList *items, *iter;
+
+  items = webkit_context_menu_get_items (context_menu);
+  for (iter = items; iter; iter = g_list_next (iter))
+    {
+      WebKitContextMenuItem *item = (WebKitContextMenuItem *) iter->data;
+
+      if (webkit_context_menu_item_get_stock_action (item) == action)
+        return item;
+    }
+
+  return NULL;
+}
+
+static gboolean
+web_view_context_menu_cb (GigWindow *self,
+                          WebKitContextMenu *context_menu,
+                          WebKitHitTestResult *hit_test_result,
+                          WebKitWebView *web_view)
+{
+  g_assert (GIG_IS_WINDOW (self));
+  g_assert (WEBKIT_IS_WEB_VIEW (web_view));
+
+  if (webkit_hit_test_result_context_is_link (hit_test_result))
+    {
+      WebKitContextMenuItem *open_link_item =
+          find_item_in_context_menu (context_menu, WEBKIT_CONTEXT_MENU_ACTION_OPEN_LINK);
+      webkit_context_menu_remove (context_menu, open_link_item);
+    }
+
+  return FALSE;
+}
+
 static void
 web_view_address_changed_cb (GigWindow *self,
                              GParamSpec *pspec,
@@ -201,6 +238,12 @@ gig_window_init (GigWindow *self)
   g_signal_group_connect_object (self->web_view_signals,
                                  "create",
                                  G_CALLBACK (web_view_create_cb),
+                                 self,
+                                 G_CONNECT_SWAPPED);
+
+  g_signal_group_connect_object (self->web_view_signals,
+                                 "context-menu",
+                                 G_CALLBACK (web_view_context_menu_cb),
                                  self,
                                  G_CONNECT_SWAPPED);
 
